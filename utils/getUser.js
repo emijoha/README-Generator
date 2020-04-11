4880b11955f2e758f4b229545a5f3b29d6bf0e9f
emi <ejhartline@gmail.com>
2020-04-11 01:38:03 -0400

    commenting added to getUser module, as well as extra error message helpfullness

--- utils/getUser.js
+++ utils/getUser.js
@@ -1,40 +1,47 @@
+// dependencies for this module
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
 async function getUser() {
   try {
+    // wait for username input to be given and store in variable
     const { username } = await inquirer.prompt({
       type: "input",
       message: "Enter your GitHub username:",
       name: "username"
     });
 
+    // store axios data in variable
     const { data } = await axios.get(
+      // use username variable to complete api url
       `https://api.github.com/users/${username}`
     );
 
+    // store needed github user info in object variable
     const gitInfo = {
       legalName: data.name,
       imageUrl: data.avatar_url,
       gitPage: data.html_url,
       gitFollowers: data.followers
     };
-
+    // stringify the object for use in json file
     const gitInfoJSON = JSON.stringify(gitInfo, null, 2);
 
     fs.writeFile("gitInfo.json", gitInfoJSON, function(err) {
       if (err) {
         throw err;
       }
-
+      // if no error, confirm that file was written
       console.log("gitInfo.json written!")
     });
-
+  
+  // catch any axios errors
   } catch (err) {
     console.log(err);
+    console.log("TIP: check your spelling")
   }
 };
 
-
+// export getUser function
 module.exports = getUser;
