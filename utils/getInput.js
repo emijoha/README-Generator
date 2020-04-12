6f474c5192a925afbe08e9c8ca719a237fe8ece6
emi <ejhartline@gmail.com>
2020-04-12 12:15:33 -0400

    re-wrote prompts to be more indicative of where their inputs will be used on the readme. renamed gitInfo.json to gitHubInfo.json.

--- utils/getInput.js
+++ utils/getInput.js
@@ -7,7 +7,7 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is the title of your project?",
+        message: "_TITLE__What is the title of your project?",
         default: "README Generator"
     },
     {
@@ -26,50 +26,50 @@ const questions = [
     {
         type: "input",
         name: "description",
-        message: "Write a short description explaining your project.",
-        default: "This file was generated using a CLI application that uses imput from prompts to automatically generate your starting README."
+        message: "_DESCRIPTION__Write a short description explaining your project.",
+        default: "A CLI application that takes the user's prompt inputs and automatically generates a well-structured initial README."
     },
     {
         type: "input",
         name: "installation",
-        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-        default: "\n  1. Clone this repo \n  2. Open the repo locally \n  3. Open terminal and run 'npm install' in the main directory \n  4. Run 'node index' \n  5. Answer prompts in the CLI"
+        message: "_INSTALLATION__What are the steps required to install your project? Provide a step-by-step description.",
+        default: "1. Clone this repo\n2. Open the repo locally\n3. Open terminal in main directory and run 'npm install'."
     },
     {
         type: "input",
         name: "usage",
-        message: "Provide instructions and examples for use.",
-        default: "When developing a new project, use this CLI application to automatically generate a well-structured README. Save time and effort! Simply answer the prompts in the CLI and copy the generate README.md into your project repo."
+        message: "_USAGE__Provide instructions and examples for use.",
+        default: "When developing a new project, use this CLI application to automatically generate a well-structured README. Save time and effort!\n1. Open terminal in main directory\n2. Run 'node index'\n3. Answer the prompts in the command line\n4. Copy the generated README into your intended project repo"
     },
     {
         type: "input",
         name: "license",
-        message: "Let your reader know what (if any) license has been applied to your project.",
-        default: "MIT"
+        message: "_LICENSE__Let your reader know what (if any) license has been applied to your project.",
+        default: "MIT License"
     },
     {
         type: "input",
         name: "contributing",
-        message: "Write a message for either welcoming or postponing contributions to your project:",
+        message: "_CONTRIBUTING__Write a message for either welcoming or postponing contributions to your project:",
         default: "Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change. Please make sure to update tests as appropriate."
     },
     {
         type: "input",
         name: "tests",
-        message: "Provide examples on how to run tests for your application.",
-        default: "You will find any developed tests in a 'tests' directory. Tests will be written using 'jest'. To run tests, make sure 'jest' is a dependency. Open terminal in the directory housing the 'test.js' files, and run 'npm run test'. This will run all test and give you the results."
+        message: "_TESTS__Provide examples on how to run tests for your application.",
+        default: "You will find any developed tests in a 'tests' directory. Tests will require 'jest'. To run tests, make sure 'jest' is installed as a dependency. Open terminal in the directory housing the 'test.js' files, and run 'npm run test'. This will run all tests and give print results."
     },
     // this messge will preceded author pic and contact info
     {
         type: "input",
         name: "questionsMessage",
-        message: "Write a message inviting readers to ask questions about your project:",
+        message: "_QUESTIONS__Write a message inviting readers to ask questions about your project:",
         default: "Feel free to contact us with any questions regarding this project!"
     },
     {
         type: "input",
         name: "email",
-        message: "Enter you email for contact info:",
+        message: "Enter your email for contact info:",
         default: "example@gmail.com"
     }
 ];
