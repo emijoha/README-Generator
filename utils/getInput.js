b64777bf7f073144dd6fa626a73c21add21ff7c0
emi <ejhartline@gmail.com>
2020-04-11 22:58:19 -0400

    gave all prompts default values

--- utils/getInput.js
+++ utils/getInput.js
@@ -8,7 +8,8 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is the title of your project?"
+        message: "What is the title of your project?",
+        default: "README Generator"
     },
     // will determine if "maintained" badge is added
     {
@@ -20,17 +21,20 @@ const questions = [
     {
         type: "input",
         name: "description",
-        message: "Write a short description explaining your project."
+        message: "Write a short description explaining your project.",
+        default: "This file was generated using a CLI application that uses imput from prompts to automatically generate your starting README."
     },
     {
         type: "input",
         name: "installation",
-        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        default: "1. Clone this repo \n 2. Open the repo locally \n 3. Open terminal and run 'npm install' in the main directory \n 4. Run 'node index' \n 5. Answer prompts in the CLI"
     },
     {
         type: "input",
         name: "usage",
-        message: "Provide instructions and examples for use."
+        message: "Provide instructions and examples for use.",
+        default: "When developing a new project, use this CLI application to automatically generate a well-structured README. Save time and effort! \n Simply answer the prompts in the CLI and copy the generate README.md into your project repo."
     },
     // will determine what license message will be passed in readme markdown
     {
@@ -49,14 +53,15 @@ const questions = [
     {
         type: "input",
         name: "tests",
-        message: "Provide examples on how to run tests for your application."
+        message: "Provide examples on how to run tests for your application.",
+        default: "You will find any developed tests in a 'tests' directory. Tests will be written using 'jest'. To run tests, make sure 'jest' is a dependency. Open terminal in the directory housing the 'test.js' files, and run 'npm run test'. This will run all test and give you the results."
     },
     // this messge will preceded author pic and contact info
     {
         type: "input",
         name: "questionsMessage",
         message: "Write a message inviting readers to ask questions about your project:",
-        default: "Please contact us with any questions about this project:"
+        default: "Feel free to contact us with any questions regarding this project!"
     }
 ];
 
